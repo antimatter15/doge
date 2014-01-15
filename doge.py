1766e5a835e183110592959c578839e66bd7bd34
Kevin Kwok <antimatter15@gmail.com>
2014-01-15 16:55:47 -0500

    doge

---
+++ doge.py
@@ -0,0 +1,27 @@
+from PIL import Image, ImageDraw, ImageFont
+import random
+import sys
+
+font = ImageFont.truetype("Comic Sans MS.ttf", 34, encoding='unic')
+
+im = Image.open('template.jpg')
+draw = ImageDraw.Draw(im)
+
+old_positions = []
+
+for arg in sys.argv:
+	pos = None
+	while pos == None:
+		pos = (random.randint(1, im.size[0] - 90), random.randint(1, im.size[1] - 30))
+		for (x, y) in old_positions:
+			if (pos[0] - x)**2 + (pos[1] - y)**2 < 100**2:
+				pos = None
+				break
+	old_positions.append(pos)
+	# color = "hsl(" + str(random.randint(1, 360)) + ", 76%, 45%)"
+	color = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
+
+	draw.text(pos, arg,  font = font, fill = color)
+
+im.show()
+
