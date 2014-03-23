f8e9227325606ecb32f57d3c87ac40a1d2240549
Cristián Rojas <injcristianrojas@gmail.com>
2014-03-23 14:17:45 -0300

    Removed doge.py from rendered strings

--- doge.py
+++ doge.py
@@ -10,18 +10,19 @@ draw = ImageDraw.Draw(im)
 old_positions = []
 
 for arg in sys.argv:
-	pos = None
-	while pos == None:
-		pos = (random.randint(1, im.size[0] - 90), random.randint(1, im.size[1] - 30))
-		for (x, y) in old_positions:
-			if (pos[0] - x)**2 + (pos[1] - y)**2 < 100**2:
-				pos = None
-				break
-	old_positions.append(pos)
-	# color = "hsl(" + str(random.randint(1, 360)) + ", 76%, 45%)"
-	color = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
+	if not 'doge.py' in arg:
+		pos = None
+		while pos == None:
+			pos = (random.randint(1, im.size[0] - 90), random.randint(1, im.size[1] - 30))
+			for (x, y) in old_positions:
+				if (pos[0] - x)**2 + (pos[1] - y)**2 < 100**2:
+					pos = None
+					break
+		old_positions.append(pos)
+		# color = "hsl(" + str(random.randint(1, 360)) + ", 76%, 45%)"
+		color = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
 
-	draw.text(pos, arg,  font = font, fill = color)
+		draw.text(pos, arg,  font = font, fill = color)
 
 im.show()
 
