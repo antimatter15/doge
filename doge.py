f549dee3e1ae6b6c7213fab764e9302a1b5e17e7
Kevin Kwok <antimatter15@gmail.com>
2014-04-03 00:00:45 -0400

    measures text and ensures no overlaps

--- doge.py
+++ doge.py
@@ -3,27 +3,47 @@
 from PIL import Image, ImageDraw, ImageFont
 import random
 import sys
+import math
 
 font = ImageFont.truetype("Comic Sans MS.ttf", 34, encoding='unic')
 
-im = Image.open('template.jpg')
+im = Image.open('template3.jpg')
 draw = ImageDraw.Draw(im)
 
-old_positions = []
+old_boxes = []
 colors = ['Red', 'Blue', 'Brown', 'Green', 'DarkViolet', 'DarkMagenta', 'Teal']
 
-for arg in sys.argv:
-	if not 'doge.py' in arg:
-		pos = None
-		while pos == None:
-			pos = (random.randint(1, im.size[0] - 90), random.randint(1, im.size[1] - 30))
-			for (x, y) in old_positions:
-				if (pos[0] - x)**2 + (pos[1] - y)**2 < 100**2:
-					pos = None
-					break
-		old_positions.append(pos)
-		color = random.choice(colors)
+def rect_dist((ax0, ay0, ax1, ay1), (bx0, by0, bx1, by1)):
+	width = max(0, max(ax0, bx0) - min(ax1, bx1))
+	height = max(0, max(ay0, by0) - min(ay1, by1))
+	return math.sqrt(width**2 + height**2)
 
-		draw.text(pos, arg,  font = font, fill = color)
+gutter = 20
+
+for arg in sys.argv:
+	if 'doge.py' in arg:
+		continue
+	
+	box = None
+	iterations = 0
+	(text_width, text_height) = draw.textsize(arg, font = font)
+	while box == None:
+		x = random.randint(gutter, im.size[0] - text_width - gutter)
+		y = random.randint(gutter, im.size[1] - text_height - gutter)
+		box = (x, y, x + text_width, y + text_height)
+
+		iterations += 1
+		if iterations > 100:
+			break
+		for other in old_boxes:
+			dist = rect_dist(box, other)
+			if dist < 100:
+				box = None
+				break
+
+	old_boxes.append(box)
+	color = random.choice(colors)
+
+	draw.text((box[0], box[1]), arg,  font = font, fill = color)
 
 im.show()
